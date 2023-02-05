b5cdb82a5d548aa6de65f4220e57363afa117c2e
Edgar Wideman <edywide@gmail.com>
2023-02-04 21:39:56 -0500

    Change to AndroidViewModel
    Signed-off-by: Edgar Wideman <edywide@gmail.com>

--- app/src/main/java/com/openautodash/MainActivityViewModel.java
+++ app/src/main/java/com/openautodash/MainActivityViewModel.java
@@ -1,8 +1,15 @@
 package com.openautodash;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
-public class MainActivityViewModel extends ViewModel {
+public class MainActivityViewModel extends AndroidViewModel {
 
 
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
 }
